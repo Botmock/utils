02cb403ac3ab2ea106eea1a4e7b5db1ef9afe579
Kevin Donahue <nonnontrivial@gmail.com>
2019-04-16 14:20:12 -0400

    fix tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,5 +1,12 @@
-const { createIntentMap } = require('../');
+const { createIntentMap, getIntermediateNodes } = require('../');
 
-test('returns a Map', () => {
+test('`createIntentMap` returns a map', () => {
   expect(createIntentMap()).toEqual(new Map());
+});
+
+test('`getIntermediateNodes` returns a function', () => {
+  expect(() => {
+    getIntermediateNodes();
+  }).not.toThrow();
+  expect(getIntermediateNodes() instanceof Function).toBeTruthy();
 });
